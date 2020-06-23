62564334beb938125d48168cb18971a8dbe35a03
CYAN4S <37798132+CYAN4S@users.noreply.github.com>
2020-06-24 00:42:15 +0900

    Add Input Manager

---
+++ Assets/Scripts/EffectsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsManager : MonoBehaviour
+{
+    public GameObject[] pressEffectObjects;
+
+    private void Start()
+    {
+        InputManager.OnPlayKeyDown += n => {
+            pressEffectObjects[n].SetActive(true);
+        };
+
+        InputManager.OnPlayKeyUp += n => {
+            pressEffectObjects[n].SetActive(false);
+        };
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+
+    public static readonly KeyCode[] playKeyCodes = { KeyCode.D, KeyCode.F, KeyCode.J, KeyCode.K };
+    public static readonly KeyCode[] speedKeyCodes = { KeyCode.T, KeyCode.Y };
+    public static Action<int> OnPlayKey = _ => {}, OnPlayKeyDown = _ => {}, OnPlayKeyUp = _ => {};
+    public static Action<int> OnSpeedKeyDown = _ => {};
+
+    private void Awake()
+    {
+        Instance = this;
+        OnPlayKey += n => {};
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < playKeyCodes.Length; i++)
+        {
+            KeyCode key = playKeyCodes[i];
+
+            if (Input.GetKey(key))
+                OnPlayKey(i);
+
+            if (Input.GetKeyDown(key))
+                OnPlayKeyDown(i);
+
+            if (Input.GetKeyUp(key))
+                OnPlayKeyUp(i);
+        }
+
+        for (int i = 0; i < speedKeyCodes.Length; i++)
+        {
+            KeyCode key = speedKeyCodes[i];
+            if (Input.GetKeyDown(key))
+                OnSpeedKeyDown(i);
+        }
+    }
+}
